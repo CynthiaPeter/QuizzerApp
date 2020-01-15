519756f4b2950c0fb2d7e5d58c2c82ab492ffaf0
Cynthia Peter <cynthiapeterpearl@gmail.com>
2020-01-15 10:50:35 +0100

    A new day

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
@@ -25,6 +26,20 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List <Icon> scoreKeeper = [
+          Icon(
+            Icons.check, color: Colors.green,
+          ),
+          Icon(
+            Icons.close, color: Colors.red,
+          ),
+          Icon( 
+            Icons.check, color: Colors.green,
+          ),
+          Icon( 
+            Icons.close, color: Colors.red,
+          )
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -62,6 +77,11 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(Icons.check, color:Colors.green,),
+                  );
+                });
               },
             ),
           ),
@@ -84,7 +104,9 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(children: scoreKeeper
+
+        )
       ],
     );
   }
